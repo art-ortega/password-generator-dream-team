0c34de5d55126527533b8087ed0d49720e302016
learn <learn@LEARN-13.local>
2020-02-06 16:17:21 -0800

    password-final

--- script.js
+++ script.js
@@ -1,51 +1,97 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordLength
-var upperCase
-var numeric
-var symbol
+// function decleration ask that prompts user how many characters they want to include in their password
 const ask = () =>{
     passwordLength = prompt ("How many characters do need in your password? (please enter a number between 8 and 128)")
 }
 
 const generatePassword = () =>{
     ask()
+    //if characters selected dont range between 8-128, ask them again until condition is no longer met
     if (passwordLength < 8 || passwordLength > 128) {
         generatePassword()
     } else {
+    // ask user if they want lowercase letters included in their password
+    lowerCase = confirm ("Do you want lowercase letters in your password?")
+    // ask user if they want to uppercase letters included in their password
     upperCase = confirm ("Do you want uppercase letters in your password?")
+    // ask user if they want to include numbers in their password
     numeric = confirm ("Do you want to include numbers in your password?")
+    // ask user if they want to include symbols in their password
     symbol = confirm ("Do you want to include symbols in your password?")
     }
-    return passwordGenerator(passwordLength, upperCase, numeric, symbol)
+    // returns the values to function generatePassword ex(10,true,true,false,true)
+    return passwordGenerator(passwordLength, lowerCase, upperCase, numeric, symbol)
 
 }
 
-const passwordGenerator = (length, upper, num, sym) => {
-        let arr1 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-        let arr2 = arr1.map(value => value.toUpperCase())
-        // console.log(arr2);
-        let arr3 = [1,2,3,4,5,6,7,8,9,0]
-        let arr4 = ["!","@","#","$","%","^","&","*","(",")"]
-        let pwArr = arr1
-        let finalArr = []
+const passwordGenerator = (length, lower, upper, num, sym) => {
+        //assign pool of characters in variable
+        let strLowerCase = "qwertyuiopasdfghjklzxcvbnm"
+        let strUpperCase = strLowerCase.toUpperCase();
+        let strNumbers = "1234567890"
+        let strSymbols = "!@#$%^&*(){}|[],."
+        let pw = ""
+        let strAll = ""
+        //set characters used count to 0
+        let charUsed = 0
+        // if users wants lowercase run code
+        if(lower) {
+            //assigns pool of lowercase letters to strAll var
+            strAll += strLowerCase
+            // picks one random letter from lowercase pool and stores it in pw var
+            pw += strLowerCase.charAt(Math.floor(Math.random()*strLowerCase.length))
+            //increments charUsed by 1
+            charUsed++
+        }
+        //if user wants uppercase letters run code
         if(upper) {
-            pwArr = pwArr.concat(arr2)
+            //assigns pool of uppercase letters to strAll var
+            strAll += strUpperCase
+            // picks a random character from uppercase pool and stores to pw var
+            pw += strUpperCase.charAt(Math.floor(Math.random()*strUpperCase.length))
+            //increments charUsed by 1
+            charUsed++
         }
+        //if user wants numbers in pass run code
         if(num) {
-            pwArr = pwArr.concat(arr3)
+            //assign pool of numbers to strAll var
+            strAll += strNumbers
+            // pick a random number from number pool and stores it in pw var
+            pw += strNumbers.charAt(Math.floor(Math.random()*strNumbers.length))
+            // incriments charused var by 1
+            charUsed++
         }
+        //if user wants symbols in their pass run code
         if(sym) {
-            pwArr = pwArr.concat(arr4)
+            //assign pool of symbols to strAll var
+            strAll += strSymbols
+            //picks random character from symbol pool and stores it in pw var
+            pw += strSymbols.charAt(Math.floor(Math.random()*strSymbols.length))
+            // incriments charused count by 1
+            charUsed++
         }
-        for(let i = 0; i < length; i++){
-            let randomIndex = Math.floor(Math.random()* pwArr.length)
-            finalArr.push(pwArr[randomIndex])
+        // iterates through usercount - the current charUsed count to generate the rest of password
+        for(let i = 0; i < length - charUsed; i++){
+            //generates random index of strAll and stores it in pw var
+            pw += strAll.charAt(Math.floor(Math.random()* strAll.length))
             }
-        return finalArr.join("")
-
-
+            //returns shuffled password using our shuffle function
+        return shuffle(pw)
 }
+// shuffle function 
+const shuffle = (string) => {
+    let array = string.split("")
+    for(let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1))
+        let tmp = array[i]
+        array[i] = array[j]
+        array[j] = tmp
+    }
+    return array.join("")
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
